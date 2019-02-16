a33e4b5f9ce4b862975e4ed545f590aedb8b5028
Palemka <47522828+Palemka@users.noreply.github.com>
2019-02-16 15:45:48 +0100

    Update guessing_game.py
    Completed game with 'play again' option. Fixed bugs, when user put incorrect options

--- guessing_game.py
+++ guessing_game.py
@@ -1,31 +1,71 @@
 import random
 print("Let\'s play a guessing game!")
-winning_option = random.randint(1,20)
-def play():
+
+def play(n=20, tries=5):
+
+  winning_option = random.randint(1,n)
   #print(winning_option)
   count = 0
-  
-  while count < 5:
-    user_choice = input("Guess number from 1 to 20. You have {0} tries left.\n".format(5-count))  
-    if int(user_choice) > 20 or int(user_choice) == 0: 
-      print("Invalid number. Please enter number from 1 to 20.")      
-    elif int(user_choice) > winning_option:
+  print("Guess number from 1 to {0}. You have {1} tries left.\n.".format(n, tries-count))
+  while count < tries:
+    user_choice = input()
+    user_choice = int(user_choice)
+    if user_choice > n or user_choice == 0: 
+      print("Invalid number. Please enter number from 1 to {0}.".format(n))      
+    elif user_choice > winning_option:
         count += 1
-        if count == 5:
+        if count == tries:
             print("You've guessed incorrectly! You are out of moves. Better luck next time!")
+            print("Do you want to try again? Y/N") 
+            playagain = input()
+            playagain = str(playagain)
+            while playagain != "Y" and playagain != "N":
+              print("Incorrect letter. Please type Y if you want to play again, or N if you don't")
+              playagain = input()
+              playagain = str(playagain)
+            if playagain == "Y":
+              play(n, tries)
+            else:
+              return
         else:
             print("You've guessed wrong! My number is lower.")
 
-    elif int(user_choice) < winning_option:
+    elif user_choice < winning_option:
         count +=1
-        if count == 5:
+        if count == tries:
             print("You've guessed incorrectly! You are out of moves. Better luck next time!")
+            print("Do you want to try again? Y/N") 
+            playagain = input()
+            playagain = str(playagain)
+            while playagain != str("Y") and playagain != str("N"):
+              print("Incorrect letter. Please type Y if you want to play again, or N if you don't")
+              playagain = input()
+              playagain = str(playagain)
+            if playagain == "Y":
+              
+              play(n, tries)
+            else:
+              return
+          
+              
         else:
             print("You've guessed wrong! My number is higher.")
     else:
         count += 1
         print("Congratulations! You've guessed my number in {0} moves!".format(count))
-        break
+        print("Do you want to try again? Y/N")
+        playagain = input()
+        playagain = str(playagain)
+        while playagain != "Y" and playagain != "N":
+              print("Incorrect letter. Please type Y if you want to play again, or N if you don't")
+              playagain = input()
+              playagain = str(playagain)
+        if playagain == "Y":
+          
+          play(n, tries)
+        else:
+          break
+        
 
 play()
             
